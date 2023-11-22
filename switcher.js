8c37e281de931b0ea83bbe0235b485e32e33dc0c
RilesH-bluesky <rileyhayden15@gmail.com>
2023-11-22 13:25:40 -0500

    Testing

--- switcher.js
+++ switcher.js
@@ -1,25 +1,34 @@
 var dark = document.getElementById("dark");
 var light = document.getElementById("light");
+// var lightCycler = document.getElementById("lightImage");
+// var darkCycler = document.getElementById("darkImage");
 dark.disabled = true 
+// darkCycler.disabled = true
 
 
 function modeSwitch() {
 
     var logo = document.getElementById("logo");
     var checkBox = document.getElementById("modeSwitcher");
-    var imageCycler = document.getElementById("imageCycler");
+    var imageCycler = document.getElementById("imageCycler")
+    // var lightCycler = document.getElementById("lightImage");
+   // var darkCycler = document.getElementById("darkImage");
 
     if (checkBox.checked == true){
         light.disabled = true;
         dark.disabled = false;
         logo.src = "images/IAmSolutionsLogoWhite.png";
-        imageCycler.src = "darkImageCycler.js"
+        imageCycler.src = "imagecyclers/darkImageCycler.js"
+        // lightCycler.disabled = true;
+       // darkCycler.disabled = false;
     } 
     else{
         light.disabled = false;
         dark.disabled = true;
         logo.src = "images/IAmSolutionsLogo.png";
-        imageCycler.src = "lightImageCycler.js"
+        imageCycler.src = "imagecyclers/lightImageCycler.js"
+        // lightCycler.disabled = false;
+       // darkCycler.disabled = true;
     }
 
   } 
